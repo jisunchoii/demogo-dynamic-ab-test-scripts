f6f664e895c48722b07c8f568d0d5e42956afb6f
jisunchoii <132889244+jisunchoii@users.noreply.github.com>
2023-05-10 12:45:14 +0900

    Update simulate.py

--- simulate.py
+++ simulate.py
@@ -1,4 +1,4 @@
 import requests
 
 for i in range(501):
-    response = requests.get('https://rgc57w9mgj.execute-api.ap-northeast-2.amazonaws.com/recommendations')
+    response = requests.get('YOUR_API_URL')
